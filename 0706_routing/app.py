ee79c2d3719b4a386f4fde5db3d18622d01e26fe
masteren <1447667558@qq.com>
2026-07-06 14:10:14 +0900

    flask/templates

---
+++ 0706_routing/app.py
@@ -0,0 +1,39 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+# ※先頭の/は必須（ルート）
+# このURLに対応する処理を
+# 関数で定義する。
+# 関数名はなんでもOK。(URLと不一致でもOK)
+# ただし、ダブりはないこと。
+def index():
+    return  'hello'
+
+# @app.route('/index2')
+# def index():
+#     return  'hello'
+# ダブりはないこと。
+
+# エンドポイント
+# URLに紐づいた処理をエンドポイントと言う。
+# 既定では関数名がエンドポイント名となる。
+# 主に後述のurl_forにて利用する。
+# また、app.routeで指定したURLのことをルール(rule)と言う。
+# flask routes コマンドで確認可能
+# ※app.pyが存在するディレクトリにて実行すること。
+
+# エンドポイント名の変更
+@app.route('/a', endpoint='b')
+def a():
+    return 'a'
+
+# endpointを付与すれば、関数名のダブルOK
+@app.route('/c', endpoint='c')
+def a():
+    return 'c'
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 5005, debug= True)
+
