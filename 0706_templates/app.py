4df92c04b1a1cdd06704db98f04f2372c0b3365c
masteren <1447667558@qq.com>
2026-07-13 15:28:31 +0900

    add_img

--- 0706_templates/app.py
+++ 0706_templates/app.py
@@ -2,6 +2,7 @@
 #  HTMLを中心に、動的なページを作成する仕組み。
 #  Flaskでは、Jinja2というテンプレートエンジンを利用。
 from flask import Flask, render_template
+from user import User
 
 app = Flask(__name__)
 
@@ -21,11 +22,33 @@ def index():
 # View(app.py)から、テンプレートファイルに、データを受け渡すことができる。
 @app.route('/pass_data')
 def pass_data():
-    from user import User
     user = User(456, 'def')
 
     # 第2引数以降にキーワード引数で受け渡す。
     return render_template('pass_data.html', id=123, name='<h1>abc</h1>', user=user)
+
+@app.route('/if_for')
+def if_for():
+    age = 18
+    colors = ['r', 'g', 'b']
+    colors_dic = {'r':'赤',
+                 'g':'緑',
+                 'b':'青',
+                 }
+    users = [
+        User(1, 'a'),
+        User(2, 'b'),
+        User(3, 'c'),
+                 ]
+    return render_template('if_for.html',
+                           age=age,
+                           colors=colors,
+                           colors_dic=colors_dic,
+                           users=users)
+
+@app.route('/img_etc')
+def img_etc():
+    return render_template('img_etc.html',)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 5005, debug= True)
